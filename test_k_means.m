5555ca2df8c9c5355493298f884f73c2608fe912
Lucas Santos <lucas26xd@gmail.com>
2019-06-22 17:08:57 -0300

    Coloquei teste de VRC, e pega o máximo VRC de 1 a 10 no número de clusters

--- test_k_means.m
+++ test_k_means.m
@@ -2,11 +2,28 @@ rng(3);
 
 centers = [1 1 1; 1 3 2; 3 3 3; 4 4 4];
 data = repmat(centers, 40, 1) + 0.4 * randn(160, 3);
+numTests = 10;
+cluster = 0;
+VRC = zeros(numTests, 1);
 
-[idx, c, vrc] = k_means(data, 4);
+for k = 1:numTests
+    [idx, c, vrc] = k_means(data, k);
+    
+    VRC(k) = vrc;
+    if vrc >= max(VRC)
+        centroide = c;
+        indices = idx;
+        cluster = k;
+    end
+    fprintf('K = %d, VRC = %.2f\n', k, vrc);
+end
 
-fprintf('VRC = %.2f\n', vrc);
+figure, scatter3(data(:, 1), data(:, 2), data(:, 3), 2, indices);
+hold on, scatter3(centroide(:, 1), centroide(:, 2), centroide(:, 3), 'x', 'black');
+title("Divisão em " + cluster + " clusters.");
 
-scatter3(data(:, 1), data(:, 2), data(:, 3), 2, idx);
-hold on;
-scatter3(c(:, 1), c(:, 2), c(:, 3), 'x', 'black');
+
+figure, plot(1:numTests, VRC, '--bo');
+hold on, plot(cluster, VRC(cluster), 'ro', 'MarkerFaceColor', 'red');
+title('Valores de VRC');
+legend('Valores de VRC', 'Máximo VRC');
